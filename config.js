e2b12f536b854ea22087171c2db142069249271c
Rafael <timbo.rafa@gmail.com>
2019-12-13 12:09:06 -0300

    create react app + scrimba react chat

---
+++ config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/409e9366-28dc-408e-ab5c-3154b983237b/token";
+const instanceLocator = "v1:us1:409e9366-28dc-408e-ab5c-3154b983237b";
+
+export { tokenUrl, instanceLocator }
