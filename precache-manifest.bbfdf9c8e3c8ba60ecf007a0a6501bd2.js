8bcd4633da398b4e2a447b1fe2bed8faac5b74ee
Rafael <timbo.rafa@gmail.com>
2019-12-13 23:18:58 -0300

    Updates

---
+++ precache-manifest.bbfdf9c8e3c8ba60ecf007a0a6501bd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "243348c9ef39d2c111e3765834bf5996",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a297469b8f846891d28e",
+    "url": "/static/css/main.e2b39846.chunk.css"
+  },
+  {
+    "revision": "c0d09fb15ad1dbc38e2d",
+    "url": "/static/js/2.51fcf632.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.51fcf632.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a297469b8f846891d28e",
+    "url": "/static/js/main.4bb2bc19.chunk.js"
+  },
+  {
+    "revision": "7f61bd5bdb7395a926ea",
+    "url": "/static/js/runtime-main.39e417cc.js"
+  }
+]);
